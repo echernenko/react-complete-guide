54b6093b5ef4f95c2e3d5f4f5b6cbf58e9f2443f
Eugene Chernenko <eugene.chernenko@booking.com>
2019-04-03 16:03:09 +0200

    learning lists and conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,40 +6,76 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Max', age: 29},
-      {name: 'Manu', age: 28},
-      {name: 'Stephanie', age: 26}
+      {id: 1, name: 'Max', age: 29},
+      {id: 2, name: 'Manu', age: 28},
+      {id: 3, name: 'Stephanie', age: 26}
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   };
 
-  switchNameHandler = () => {
-    // console.log('Was clicked!');
+  switchNameHandler = (event, id) => {
+    
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const persons = [...this.state.persons];
+    persons[personIndex].name = event.target.value;
     this.setState({
-      persons: [
-        {name: 'Maximilian', age: 29},
-        {name: 'Manu', age: 28},
-        {name: 'Stephanie', age: 27}
-      ]
-    })
+      persons: persons
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              name={person.name} 
+              age={person.age} 
+              key={person.id}
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.switchNameHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
-    // return React.createElement('div',
-    //     {
-    //       className: 'App'
-    //     },
-    //     React.createElement('h1', null, 'Does this work now?')
-    // );
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Person.css';
+
 
 const person = (props) => {
    return (
-       <div>
-         <p>I am a {props.name} and I am {props.age} years old.</p>
+       <div className="Person">
+         <p onClick={props.click}>I am a {props.name} and I am {props.age} years old.</p>
          <p>{props.children}</p>
+         <p>
+           <input type="text" value={props.name} onChange={props.changed} />
+         </p>
        </div>
    );
 };
